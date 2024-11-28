e480318dc227c29b46a29677b40d1c0cfbe64eb0
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2024-11-28 19:53:50 +0600

    изменил создание статьи, добавил выбор жанра

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from webapp.models import Article
+from webapp.models import Article, Genre
 
 
 def index_view(request):
@@ -14,10 +14,17 @@ def article_view(request, *args, pk, **kwargs):
 
 def article_create_view(request):
     if request.method == 'GET':
-        return render(request, 'article_create.html')
+        genres = Genre.objects.all()
+        return render(request, 'article_create.html', context={'genres': genres})
     elif request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
         author = request.POST.get('author')
-        article = Article.objects.create(title=title, content=content, author=author)
+        genre_id = request.POST.get('genre_id')
+        # genre = get_object_or_404(Genre, pk=genre_id)
+        article = Article.objects.create(title=title,
+                                         content=content,
+                                         # genre=genre,
+                                         genre_id=genre_id,
+                                         author=author)
         return redirect('article_detail', pk=article.id)
