4aa763ed2f951d1cb4679063ff24035b7be7e6f6
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2024-12-16 20:23:58 +0600

    Реализовал поиск на главной странице

--- source/webapp/forms/__init__.py
+++ source/webapp/forms/__init__.py
@@ -1,1 +1,2 @@
 from .articles import ArticleForm
+from .search_form import SearchForm
--- source/webapp/views/articles.py
+++ source/webapp/views/articles.py
@@ -1,6 +1,8 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils.http import urlencode
 from django.views.generic import View, TemplateView, FormView, ListView
-from webapp.forms import ArticleForm
+from webapp.forms import ArticleForm, SearchForm
 from webapp.models import Article
 
 
@@ -11,6 +13,34 @@ class ArticleListView(ListView):
     ordering = ['-created_at']
     paginate_by = 3
     paginate_orphans = 2
+
+    def dispatch(self, request, *args, **kwargs):
+        self.search_form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.search_form.is_valid():
+            return self.search_form.cleaned_data['search']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            queryset = queryset.filter(Q(title__icontains=self.search_value) | Q(author__icontains=self.search_value))
+
+        return queryset
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = self.search_form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+            context['search_value'] = self.search_value
+        return context
+
 
 
 class ArticleDetailView(TemplateView):
